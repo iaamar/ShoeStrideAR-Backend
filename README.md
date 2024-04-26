b6a86863b2b7ca12177efc77c8b67f41e462ce54
Amar Nagargoje <amargithub@gmail.com>
2024-04-25 22:47:11 -0400

    added api for stripe payment

---
+++ README.md
@@ -0,0 +1,44 @@
+# ShoeStrideARBackend
+
+This is the backend component of the ShoeStrideAR app. It provides the necessary APIs and services to support the augmented reality features of the app.
+
+## Features
+
+- API endpoints for retrieving shoe data
+- Integration with shoe database
+- Authentication and authorization mechanisms
+- Error handling and logging
+
+## Installation
+
+1. Clone the repository:
+
+    ```bash
+    git clone https://github.com/your-username/ShoeStrideARBackend.git
+    ```
+
+2. Install the dependencies:
+
+    ```bash
+    npm install
+    ```
+
+3. Configure the environment variables:
+
+    - Create a `.env` file in the root directory.
+    - Add the necessary environment variables (e.g., database connection details, API keys, etc.).
+
+4. Start the server:
+
+    ```bash
+    npm start
+    ```
+
+## Usage
+
+- Make API requests to the provided endpoints to retrieve payment data.
+- Ensure proper authentication and authorization for accessing protected routes.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvements, please open an issue or submit a pull request.
---
+++ index.js
@@ -0,0 +1,43 @@
+const cors = require("cors");
+const express = require("express");
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/create-payment-intent", async (req, res) => {
+  const customer = await stripe.customers.create({
+    email: req.body.email,
+  });
+  const ephemeralKey = await stripe.ephemeralKeys.create(
+    { customer: customer.id },
+    { apiVersion: "2020-08-27" }
+  );
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: req.body.amount,
+    currency: "usd",
+    customer: customer.id,
+    automatic_payment_methods: {
+      enabled: true,
+      
+    },
+  });
+
+  res.json({
+    clientSecret: paymentIntent.client_secret,
+    ephemeralKey: ephemeralKey.secret,
+    customerId: customer.id,
+    paymentIntentId: paymentIntent.id,
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  });
+});
+
+app.listen(process.env.PORT, "127.0.0.1", () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
